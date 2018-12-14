b911375318d70e1554f65b688d2e527dc37549f7
TraumaEnSOes <traumaensoes@gmail.com>
2018-12-14 08:47:37 +0100

    Poco a poco

--- src/rpcchild.js
+++ src/rpcchild.js
@@ -1,38 +1,40 @@
 const { EventEmitter } = require( 'events' );
-const JsonRpc = require( './jsonrpc.js' );
 const { CreateInferface } = require( 'readline' );
-
-class FakeLogger {
-  emerg( ) { }
-  alert( ) { }
-  crit( ) { }
-  err( ) { }
-  warning( ) { }
-  notice( ) { }
-  info( ) { }
-  debug( ) { }
-}
+const { exec: Exec, execFile: ExecFile, fork: Fork, spawn: Spawn } = require( 'child_process' );
+const JsonRpc = require( './jsonrpc.js' );
 
 class RpcChild extends EventEmitter {
+  get userData( ) { return this.$userData; }
+  // Total de mensajes enviados.
+  get sends( ) { return this.$sendsCount; }
+  // Total de mensajes recibidos.
+  get receiveds( ) { return this.$receivedsCount; }
   /**
-   * @param {string} path - Al recibir entradas por 'stdout' o 'stderr', se envia esto como segundo argumento del evento.
-   *                        También se envía al logger al recibir logs desde el hijo.
    * @param {*} child - Hijo, tal y como lo devuelven las funciones de 'child_process'.
-   * @param {*} logger - Instancia de clase logger, a la que se enviaran los logs.
+   * @param {*} [userData] - No usado internamente. Para colocar cualquier dato que sea necesario.
    */
-  constructor( path, child, logger ) {
+  constructor( child, userData ) {
     if( ( logger === null ) || ( logger == undefined ) ) logger = new FakeLogger( );
 
     super( );
 
-    this.$child = child;
-    this.$stdout = CreateInferface( child.stdout );
-    this.$stderr = CreateInterface( child.stderr );
+    this.$userData = userData;
+    this.$stdin = child.stdin;
+    this.$stdout = CreateInferface( {
+      input: child.stdout,
+      terminal: false
+    } );
+    this.$stderr = CreateInterface( {
+      input: child.stderr,
+      terminal: false
+    } );
     this.$queue = { };
     this.$path = path;
-    this.$logger = logger;
     this.$id = 0;
+    this.$sendsCount = 0;
+    this.$receivedsCount = 0;
 
+    child.on( 'exit', ( code, signal ) => $onChildExit( code, signal ) );
     this.$stdout.on( 'line', ( line ) => this.$onStdout( line ) );
     this.$stderr.on( 'line', ( line ) => this.emit( 'stderr', line, this ) );
   }
@@ -48,32 +50,34 @@ class RpcChild extends EventEmitter {
     return retId;
   }
 
+  $send( consume, produce, id, jsonObject ) {
+    var work;
+
+    if( id in this.$queue ) throw Error( '"id" exists yet' );
+    if( !( consume || produce ) ) throw Error( '"consume" and "produce", both 0' );
+
+    work = [
+      consume ? setTimeout( consume * 1000, ( ) => this.$timeoutConsume ) : false,
+      produce ? setTimeout( produce * 1000, ( ) => this.$timeoutProduce ) : false
+    ];
+
+    this.$queue[id] = work;
+    ++this.$sendsCount;
+    this.$stdin.write( jsonObject, null, ( ) => this.$ontimeConsume( id ) );
+  }
+
   notify( consume, methodName, args ) {
     var id = this.$nextId,
-        jsonObject = JsonRpc.makeNotify( methodName, args ),
-        work = [
-          consume ? setTimeout( consume * 1000, ( ) => this.$timeoutConsume( id ) ) : false,
-          false
-        ];
-        
-    this.$queue[id] = work;
-    this.child.stdin.write( jsonObject, ( ) => this.$ontimeConsume( id ) );
+        jsonObject = JsonRpc.makeNotify( methodName, args );
 
-    return id;
+    return this.$send( consume, 0, id, jsonObject );
   }
 
   request( consume, produce, methodName, args ) {
     var id = this.$nextId( ),
-        jsonObject = JsonRpc.makeRequest( methodName, args ),
-        work = [
-          consume ? setTimeout( consume * 1000, ( ) => this.$timeoutConsume( id ) ) : false,
-          produce ? setTimeout( produce * 1000, ( ) => this.$timeoutProduce( id ) ) : false
-        ];
-  
-    this.$queue[id] = work;
-    this.$child.stdin.write( jsonObject, ( ) => this.$ontimeConsume( id ) );
+        jsonObject = JsonRpc.makeRequest( methodName, args );
 
-    return id;
+    return this.$send( consume, produce, id, jsonObject );
   }
 
   clearAll( ) {
@@ -92,6 +96,7 @@ class RpcChild extends EventEmitter {
     }
   }
 
+  // Quita un 'id' de la cola, limpiando sus timeouts.
   clear( id ) {
     var work = this.$queue[id];
 
@@ -106,6 +111,7 @@ class RpcChild extends EventEmitter {
     delete this.$queue[id];
   }
 
+  // Se realizó el 'consume' dentro del plazo.
   $ontimeConsume( id ) {
     var work = this.$queue[id];
 
@@ -117,18 +123,19 @@ class RpcChild extends EventEmitter {
     if( !( work[1] ) ) delete this.$queue[id];
   }
 
+  // 'timeout' del consume. El hijo ha tardado mucho en obtener nuestros datos.
   $timeoutConsume( id ) {
     this.$queue[id][0] = false;
     this.emit( 'consumeTimeout', id, this );
   }
 
+  // 'timeout' del produce. El hijo ha tardado mucho en responder.
   $timeoutProduce( id ) {
-    var work = this.$queue[id];
-
     this.clear( id );
     this.emit( 'produceTimeout', id, this );
   }
 
+  // Al recibir una línea desde la salida estandar del hijo.
   $onStdout( line ) {
     var jsonObject,
         rpcType;
@@ -146,34 +153,70 @@ class RpcChild extends EventEmitter {
 
     if( rpcType !== undefined ) {
       // Error al parsear el JSON.
-      this.emit( 'parseError', rpcType );
+      // Lanzamos la señal.
+      this.emit( 'stdout', line, rpcType, this );
       return;
     }
 
     rpcType = JsonRpc.getType( jsonObject );
     if( !JsonRpc.validTypes.includes( rpcType ) ) {
-      this.emit( 'parseError', rpcType );
+      // No es un JSONRPC válido.
+      this.emit( 'stdout', line, rpcType, this );
       return;
     }
   
     // Si llegamos aquí, es un JSONRPC válido.
-    // Si tiene 'id', la quitamos de la cola.
-    if( 'id' in jsonObject ) this.clear( jsonObject.id );
+    
+    if( 'id' in jsonObject ) {
+      ++this.$receivedsCount;
 
-    // Por último, emitimos la señal.
-    emit( rpcType, jsonObject, this );
+      // Si tiene 'id', la quitamos de la cola.
+      this.clear( jsonObject.id );
+      // Y por último, emitimos la señal.
+      emit( rpcType, jsonObject, this );
+    } else {
+      // No está en la cola. ¿ Llegó fuera de tiempo ?
+      this.emit( 'unexpected', jsonObject, this );
+    }
   }
 
   static exec( command, options ) {
+    if( ( options === null ) && ( options === undefined ) ) options = { }
+
+    // Sobreescribimos las opciones que necesitamos.
+    options.windowsHide = true;
+
+    return new RpcChild( Exec( command, options ), options.userData );
   }
 
-  static execFile( command, args, options ) {
+  static execFile( file, args, options ) {
+    if( ( options === null ) && ( options === undefined ) ) options = { }
+
+    // Sobreescribimos las opciones que necesitamos.
+    options.windowsHide = true;
+
+    return new RpcChild( ExecFile( file, args, options ), options.userData );
   }
 
   static fork( modulePath, args, options ) {
+    if( ( options === null ) && ( options === undefined ) ) options = { }
+
+    // Sobreescribimos las opciones que necesitamos.
+    options.windowsHide = true;
+    options.stdio = 'pipe';
+
+    return new RpcChild( Fork( modulePath, args, options ), options.userData );
   }
 
   static spawn( command, args, options ) {
+    if( ( options === null ) && ( options === undefined ) ) options = { }
+
+    // Sobreescribimos las opciones que necesitamos.
+    options.windowsHide = true;
+    options.stdio = 'pipe';
+    options.detached = false;
+
+    return new RpcChild( Spawn( command, args, options ), options.userData );
   }
 };
 
